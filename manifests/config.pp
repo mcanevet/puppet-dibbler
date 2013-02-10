9e4d86a060bd8764107db8f0d4fe9abf70c9a61f
Mickaël Canévet <mickael.canevet@gmail.com>
2013-02-10 09:40:37 +0000

    Lint dibbler::config

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,10 +3,10 @@ class dibbler::config {
   include concat::setup
 
   concat {'/etc/dibbler/server.conf':
-     owner   => root,
-     group   => root,
-     mode    => '0644',
-   }
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+  }
 
   concat::fragment {'dibbler.conf-global':
     target  => '/etc/dibbler/server.conf',
